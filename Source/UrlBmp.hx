676eb66562fd9137c80f8f48c94aa22407c671f9
Daniel Uranga <duranga@dokkogroup.com.ar>
2015-03-27 17:57:21 -0300

    .

--- Source/UrlBmp.hx
+++ Source/UrlBmp.hx
@@ -8,24 +8,11 @@ import flash.utils.ByteArray;
 
 class UrlBmp extends Bmp {
 	
-	public var size(default, set) : Float;
 	public var url(default, set) : String;
 
 	public function new() {
 		super();
 		this.autoSize = true;
-		this.size = 100;
-	}
-
-	function set_size(size : Float) : Float {
-		this.size = size;
-		var gfx = this.graphics;
-		gfx.clear();
-		gfx.beginFill(0x222222);
-		gfx.drawRect(0, 0, size, size);
-		gfx.endFill();
-		_width = _height = size;
-		return size;
 	}
 
 	function set_url(url : String) : String {
@@ -51,16 +38,6 @@ class UrlBmp extends Bmp {
 		this.bitmapData = bmpData;
 
 		this.refresh();
-
-		if (bmpData.width!=0) {
-			this.scaleX = size / bmpData.width;
-		}
-
-		if (bmpData.height!=0) {
-			this.scaleY = size / bmpData.height;
-		}
-		_width = bmpData.width * scaleX;
-		_height = bmpData.height * scaleY;
 
 	}
 
